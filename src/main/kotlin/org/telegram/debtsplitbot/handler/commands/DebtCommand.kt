b8748ab44a1b4e04306488189e735c4701a69b2d
evser <evser11@yandex.ru>
2019-05-08 20:37:16 +0100

    Update DebtCommand.kt

--- src/main/kotlin/org/telegram/debtsplitbot/handler/commands/DebtCommand.kt
+++ src/main/kotlin/org/telegram/debtsplitbot/handler/commands/DebtCommand.kt
@@ -26,7 +26,7 @@ class DebtCommand(handler: TextMessageHandler) : Command(handler) {
         }
         if (showResult) {
             val resultCommand = ResultCommand(handler)
-            resultCommand.execute(Commands.RESULT)
+            resultCommand.execute(Commands.RESULT + " $chatContext.currentCurrency")
         }
 
         return false
